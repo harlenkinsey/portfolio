8c7efdf492351936cc0b5f6728fc6306a0a7e53d
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-08-18 00:13:04 -0500

    Added Carousel and CarouselItem
    - Removed ProjectBlock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from "./components/Header";
 import Parallax from "./components/Parallax";
-import ProjectBlock from './components/ProjectBlock';
+import Carousel from './components/Carousel';
+import CarouselItem from './components/CarouselItem';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
 
 function App() {
+
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
+
+  const handleResize = () => {
+    setDimensions({
+      height: window.innerHeight,
+      width: window.innerWidth
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => { window.removeEventListener('resize', handleResize) }
+  });
+
   return (
     <div>
       <Header />
@@ -15,6 +35,7 @@ function App() {
       <Parallax />
       <ProjectBlock />
       <Parallax />
+
     </div>
   );
 }
