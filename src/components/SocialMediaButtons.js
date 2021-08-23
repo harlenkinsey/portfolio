3e0ee4ae9120059786cc208987d426d4bdf6fb8c
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-08-23 10:01:29 -0500

    updated social media links

--- src/components/SocialMediaButtons.js
+++ src/components/SocialMediaButtons.js
@@ -8,7 +8,7 @@ const SocialMediaButtons = () => {
                 <a href='https://www.linkedin.com/in/harlen-kinsey-9075291b1' target='_blank' className='btn-floating btn-large waves-effect waves-light black'><i className='fab fa-linkedin'></i></a>
             </li>
             <li className='social-icon-subsequent'>
-                <a href='https://github.com/harlenkinsey60' target='_blank' className='btn-floating btn-large waves-effect waves-light black'><i className='fab fa-github'></i></a>
+                <a href='https://github.com/harlenkinsey' target='_blank' className='btn-floating btn-large waves-effect waves-light black'><i className='fab fa-github'></i></a>
             </li>
         </ul>
     )
