4d2987b80d42a4dde4bc34d4980635cc3069a189
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-08-23 10:48:32 -0500

    updated cards

--- src/components/Background.js
+++ src/components/Background.js
@@ -16,7 +16,7 @@ const Background = () => {
         <div className='background'>
             <Carousel items={[ 
                 <CarouselItem title='Contact Keeper' tags={['JavaScript', 'HTML', 'CSS', 'React', 'MERN']} description='A full-stack application built using React and Express.' link='https://serieux-croissant-39457.herokuapp.com/login'/>, 
-                <CarouselItem title='Github Finder' tags={['JavaScript', 'HTML', 'CSS', 'React', 'Redux', 'Materialize']} description='A full-stack application built using React, Redux, and Express.' link='https://githubfinder901237.netlify.app'/>,
+                <CarouselItem title='Github Finder' tags={['JavaScript', 'React', 'Redux', 'Materialize']} description='A full-stack application built using React, Redux, and Express.' link='https://githubfinder901237.netlify.app'/>,
                 <CarouselItem title='Star Destroyer' tags={['Android', 'Godot', 'GDScript', 'Python']} description='A small 2D game built with Godot for Android.' link='https://play.google.com/store/apps/details?id=com.lonepixel.studios&hl=en_US'/>
             ]}/>
 
