41984ca02b77f28b6dd2135d7c4efe4503212b18
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-09-29 06:31:59 -0500

    Updated card order and contents

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -8,13 +8,13 @@ const CardContainer = () => {
         <div className='background'>
             <div className='row card-container'>
                 <div className='col s12 m6 l4'>
-                    <ProjectCard title='Contact Keeper' tags={['JavaScript', 'HTML', 'CSS', 'React', 'MERN']} description='A full-stack application built using React and Express.' link='https://serieux-croissant-39457.herokuapp.com/login'/>
-                </div>
-                <div className='col s12 m6 l4'>
-                    <ProjectCard title='Github Finder' tags={['JavaScript', 'React', 'Redux', 'Materialize']} description='A full-stack application built using React, Redux, and Express.' link='https://githubfinder901237.netlify.app'/>
+                    <ProjectCard title='Github Finder' tags={['JavaScript', 'React', 'Redux', 'Materialize']} description='A full-stack application that uses the GitHub API to search for user profiles.' link='https://githubfinder901237.netlify.app'/>
                 </div>
                 <div className='col s12 m6 l4'>
                     <ProjectCard title='Star Destroyer' tags={['Android', 'Godot', 'GDScript', 'Python']} description='A small 2D game built with Godot for Android.' link='https://play.google.com/store/apps/details?id=com.lonepixel.studios&hl=en_US'/>
+                </div>
+                <div className='col s12 m6 l4'>
+                    <ProjectCard title='Contact Keeper' tags={['JavaScript', 'HTML', 'CSS', 'React', 'MERN']} description='A full-stack application used to record and manage contacts. (may load slowly)' link='https://serieux-croissant-39457.herokuapp.com/login'/>
                 </div>
             </div>
             
