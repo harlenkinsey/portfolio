1403803414515dc6175ba06df02c4761221435ec
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-12-12 10:05:51 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => {
         <div className='row'>
             <div className='col s12'>
                 <h3 className='center-align'><span className='highlight'>Hello;</span></h3>
-                <p className='center-align'>I'm Harlen, and I'm a software engineer.</p>
             </div>
         </div>
     )
