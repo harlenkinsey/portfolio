2cb44817761ed85a7e8bf0c730cdf80369c98786
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-06-24 06:04:16 -0500

    added home component for routing

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../components/Header';
+import CardContainer from '../components/CardContainer';
+import Footer from '../components/Footer';
+
+const Home = () => {
+
+    return (
+        <div>
+      
+            <Header />
+            <CardContainer />
+            <Footer />
+
+        </div>
+    )
+}
+
+export default Home
