2757232f4529aaa2e456356066c540f67f99412e
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-06-24 06:03:52 -0500

    Update CardContainer.js

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,19 +1,13 @@
 import React, { useEffect } from 'react';
-import ProjectCard from './ProjectCard';
+import PostCard from './PostCard';
 
 const CardContainer = () => {
 
     return (
         <div className='background'>
             <div className='row card-container'>
-                <div className='col s12 m6 l4'>
-                    <ProjectCard title='Github Finder' description='A full-stack application that uses the GitHub API to search for user profiles.' link='https://githubfinder901237.netlify.app'/>
-                </div>
-                <div className='col s12 m6 l4'>
-                    <ProjectCard title='Star Destroyer' description='A small 2D game built with Godot for Android.' link='https://play.google.com/store/apps/details?id=com.lonepixel.studios&hl=en_US'/>
-                </div>
-                <div className='col s12 m6 l4'>
-                    <ProjectCard title='Contact Keeper' description='A full-stack application used to record and manage contacts. (may load slowly)' link='https://serieux-croissant-39457.herokuapp.com/login'/>
+                <div className='col s12'>
+                    <PostCard title='Hello World!' description='Started the blog!' link='/hello-world' date='Jun 24, 2022'/>
                 </div>
             </div>
 
