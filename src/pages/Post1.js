a0efe880b0beb75d818aa10539957d191b345e11
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-06-24 06:04:36 -0500

    added first post blueprint

---
+++ src/pages/Post1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Post1 = () => {
+
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Post1
