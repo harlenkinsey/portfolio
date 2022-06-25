bbe84d6af19902cfdfb421bf9d3b897526edbf18
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-06-24 22:36:33 -0500

    added height correction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <div className='row black footer'>
-            <div className='col s6 copyright'>
-                <b><p className='white-text'>Harlen Kinsey <i className='fas fa-copyright'></i> 2022</p></b>
+    //used to correct footer height if body height doesn't cover screen
+    let windowHeight = window.innerHeight;
+    let bodyHeight = window.document.body.offsetHeight;
+    let correction = windowHeight - bodyHeight;
+
+    if(bodyHeight < windowHeight){
+        return (
+            <div className='row black footer' style={{height: correction}}>
+                <div className='col s6 copyright'>
+                    <b><p className='white-text'>Harlen Kinsey <i className='fas fa-copyright'></i> 2022</p></b>
+                </div>
+                <div className='col s6 right-align'>
+                    <p className='white-text'>background image: u/astrellon3</p>
+                </div>
             </div>
-            <div className='col s6 right-align'>
-                <p className='white-text'>background image: u/astrellon3</p>
+        )
+    }
+    else {
+        return (
+            <div className='row black footer'>
+                <div className='col s6 copyright'>
+                    <b><p className='white-text'>Harlen Kinsey <i className='fas fa-copyright'></i> 2022</p></b>
+                </div>
+                <div className='col s6 right-align'>
+                    <p className='white-text'>background image: u/astrellon3</p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
 
 export default Footer
