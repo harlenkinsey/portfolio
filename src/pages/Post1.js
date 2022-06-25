04e9e30d891adeefbd6801d7d2a082a0a1c2cb9c
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-06-25 07:16:11 -0500

    added first post data

--- src/pages/Post1.js
+++ src/pages/Post1.js
@@ -7,6 +7,29 @@ const Post1 = () => {
     return (
         <div>
             <Header />
+            <div className='background'>
+                <div className='post-container white-text center-align'>
+                    <div className='row '>
+                        <div className='col s12'>
+                            <h1>Hello World!</h1>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col s3 border-round' style={{backgroundColor: 'black'}}>
+                            <p>June 24, 2022</p>
+                            <p>Updated June 25, 2022</p>
+                        </div>
+                        <div className='col s1'>
+
+                        </div>
+                        <div className='col s8 border-round' style={{backgroundColor: 'black'}}>
+                        <h4>Purpose</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
             <Footer />
         </div>
     )
