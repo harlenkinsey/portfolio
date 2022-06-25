43290d0778f2bd17d1d96b7186b0a410c1f83372
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-06-24 23:33:25 -0500

    refactored rendering logic
    - now uses react conditional rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,60 +5,39 @@ const Header = () => {
 
     let width = window.innerWidth;
 
-    if (width > 768) {
-        return (
-            <div className='row header-border black'>
-                <div className='row valign-wrapper'>
-    
-                    <div className='col l6 left-align'>
-                        <div className='left-align valign-wrapper'>
-                            <li className='header-text'>
-                                <h3><span className='highlight'>Portfolio;</span></h3>
-                            </li>
-                            <li className='blog-button'>
-                                <a href='https://blog.harlenkinsey.com' className='btn-floating btn-large waves-effect waves-light white'><i className='fas fa-rocket black'></i></a>
-                            </li>
-                        </div>
+    return (
+        <div className='row header-border black'>
+            <div className='row valign-wrapper'>
+                <div className='col l6 left-align'>
+                    <div className='left-align valign-wrapper'>
+                        <li className='header-text'>
+                            <h3><span className='highlight'>Portfolio;</span></h3>
+                        </li>
+                        <li className='blog-button'>
+                            <a href='https://blog.harlenkinsey.com' className='btn-floating btn-large waves-effect waves-light white'><i className='fas fa-rocket black'></i></a>
+                        </li>
                     </div>
-                    
-                    <div className='col l6'>
-                        <div className='right-align'>
-                            <li className='header-button-initial'>
-                                <a href='https://www.linkedin.com/in/harlen-kinsey-9075291b1' target='_blank' rel="noopener noreferrer" className='btn-floating btn-large waves-effect waves-light white'><i className='fab fa-linkedin black'></i></a>
-                            </li>
-                            <li className='header-button-subsequent'>
-                                <a href='https://github.com/harlenkinsey' target='_blank' rel="noopener noreferrer" className='btn-floating btn-large waves-effect waves-light white'><i className='fab fa-github black'></i></a>
-                            </li>
-                        </div>
-                    </div>
-    
                 </div>
-            </div>
-        )
-    } else {
-        return (
-            <div className='row header-border black'>
-                <div className='row valign-wrapper'>
-    
-                    <div className='col l6 left-align'>
-                        <div className='left-align valign-wrapper'>
-                            <li className='header-text'>
-                                <h3><span className='highlight'>Portfolio;</span></h3>
-                            </li>
-                            <li className='blog-button'>
-                                <a href='#' className='btn-floating btn-large waves-effect waves-light white'><i className='fas fa-rocket black'></i></a>
-                            </li>
-                        </div>
+                
+                <div className='col l6 right-align'>
+                    { width >= 768 ?
+
+                    <div>
+                        <li className='header-button-initial'>
+                            <a href='https://www.linkedin.com/in/harlen-kinsey-9075291b1' target='_blank' rel="noopener noreferrer" className='btn-floating btn-large waves-effect waves-light white'><i className='fab fa-linkedin black'></i></a>
+                        </li>
+                        <li className='header-button-subsequent'>
+                            <a href='https://github.com/harlenkinsey' target='_blank' rel="noopener noreferrer" className='btn-floating btn-large waves-effect waves-light white'><i className='fab fa-github black'></i></a>
+                        </li>
                     </div>
-    
-                    <div className='col l6 right-align'>
-                        <Dropdown />
-                    </div>
-    
+
+                    : <Dropdown />}
                 </div>
+
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 export default Header
