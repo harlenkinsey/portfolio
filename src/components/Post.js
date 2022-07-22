14c085bb96b775d461c2aee5c4a5632633d8f995
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-07-22 08:00:09 -0500

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,7 @@ const Post = ({ title, date, update, sections, texts }) => {
 
     return (
         <div className='background'>
+<<<<<<< Updated upstream
             <div className='post-container'>
                 <div className='row section-wrapper'>
                     <h1>{title}</h1>
@@ -41,13 +42,36 @@ const Post = ({ title, date, update, sections, texts }) => {
                                 <div>
                                     <div className='white-text' id={section}>
                                         <h1>{section}</h1>
+=======
+            <div className='post-container container'>
+
+                <div className='row white-text center-align background-black'>
+                    <div className='col s8'></div>
+                    <h1>{title}</h1>
+                    <p>{date}</p>
+                    <p>{update}</p>
+                </div>
+
+                <div style={{padding:'20px 0px'}} className='row'>
+
+                    <div className='white-text center-align col s8'>
+                        {sections.map((section, index) => {
+                            return (
+                                <div>
+                                    <div className='white-text background-black'>
+                                        <h1 className='border-bottom'>{section}</h1>
+>>>>>>> Stashed changes
                                         <p>{texts[index]}</p>
                                     </div>
                                 </div>
                             )
                         })}
                     </div>
+<<<<<<< Updated upstream
                     
+=======
+
+>>>>>>> Stashed changes
                 </div>
             </div>
         </div>
