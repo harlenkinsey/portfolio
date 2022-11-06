15d06db27c6bc075327007c7bcfb20067c22de99
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-11-06 01:12:03 -0500

    added project text and fixed spacing

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -6,13 +6,14 @@ const CardContainer = () => {
     return (
         <div className='background valign-wrapper'>
             <div className='row card-container'>
-                <div className='col s12 m6 l4'>
+                <h3><span className='highlight'>Projects;</span></h3>
+                <div className='col s12 m6 l4' style={{ padding: 0 }}>
                     <ProjectCard title='Github Finder' description='A full-stack application that uses the GitHub API to search for user profiles.' link='https://githubfinder901237.netlify.app'/>
                 </div>
-                <div className='col s12 m6 l4'>
+                <div className='col s12 m6 l4' style={{ padding: 0 }}>
                     <ProjectCard title='Star Destroyer' description='A small 2D game built with Godot for Android.' link='https://play.google.com/store/apps/details?id=com.lonepixel.studios&hl=en_US'/>
                 </div>
-                <div className='col s12 m6 l4'>
+                <div className='col s12 m6 l4' style={{ padding: 0 }}>
                     <ProjectCard title='Contact Keeper' description='A full-stack application used to record and manage contacts. (may load slowly)' link='https://serieux-croissant-39457.herokuapp.com/login'/>
                 </div>
             </div>
