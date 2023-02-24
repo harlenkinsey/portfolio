82922c0d166398a1906f1033be933aa6f4ae9411
Harlen Kinsey <harlenkinsey60@gmail.com>
2023-02-24 16:17:48 -0600

    changed card styling

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
-const ProjectCard = ({ title, description, link }) => {
+const ProjectCard = ({ title, description, link, gif, credit }) => {
 
     return (
         <div className='card black center'>
-            <div className='card-content white-text'>
-
-                <div className='row project-card-header valign-wrapper'>
-                    <div className='col s8'>
-                        <span className='card-title'>{title}</span>
-                    </div>
-                    <div className='col s4' style={{ marginBottom: 10 }}>
-                        <a href={link} className='btn-floating white btn-large'><i className='fas fa-rocket' style={{color: 'black'}}></i></a>
-                    </div>
+            <div className='card-image'>
+                <div>
+                    <a href={credit}>
+                        <img src={gif} height='200px' />
+                    </a>
+                    <span className='card-title'>{title}</span>
                 </div>
-                <p className='project-card-description'>{description}</p>
-                
+            </div>
+            <div className='card-content white-text'>
+                <p>{description}</p>
+            </div>
+            <div className='card-action'>
+                <a href={link}>Explore</a>
             </div>
         </div>
     )
