456702933eecd06b3637087e7c55abf6fad17c1b
Harlen Kinsey <harlenkinsey60@gmail.com>
2023-02-24 20:50:47 -0600

    added privacy policy

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
+import PrivacyPolicy from './PrivacyPolicy';
 
 import DonutGif from '../gifs/donut.gif';
 import IDKGif from '../gifs/idk.gif';
@@ -14,7 +15,7 @@ const CardContainer = () => {
                 <div className='col s12 m6 l4'>
                     <ProjectCard 
                         title='IT Logger' 
-                        description='A full-stack application that utilizes Firebase and Redux to mimic an application that an IT department might use to manage issues.' 
+                        description={<p>A full-stack application that utilizes Firebase and Redux to mimic an application that an IT department might use to manage issues.</p>}
                         link='https://prismatic-kitten-3560cf.netlify.app/'
                         gif={WavesGif}
                         credit='https://i.gifer.com/tsu.gif'
@@ -23,17 +24,18 @@ const CardContainer = () => {
                 <div className='col s12 m6 l4'>
                     <ProjectCard 
                         title='Star Destroyer' 
-                        description='A small 2D game built with GDScript and the Godot Engine for Android.' 
+                        description={<p>A small 2D game built with GDScript and the Godot Engine for Android.</p>}
                         link='https://play.google.com/store/apps/details?id=com.lonepixel.studios&hl=en_US'
                         gif={DonutGif}
                         credit='https://i.gifer.com/4l81.gif'
-                        
+                        privacyPolicy={<PrivacyPolicy/>}  
                     />
+                    
                 </div>
                 <div className='col s12 m6 l4'>
                     <ProjectCard 
                         title='Github Finder' 
-                        description='A full-stack application that uses the GitHub API to search for user profiles.' 
+                        description={<p>A full-stack application that uses the GitHub API to search for user profiles.</p>} 
                         link='https://githubfinder901237.netlify.app'
                         gif={IDKGif}
                         credit='https://alcrego.tumblr.com/post/684371504421568512/this-little-creature-appeared-while-working-on-an'
