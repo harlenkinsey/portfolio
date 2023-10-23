2a05415edfce82154d9305d685fff3ae03015d95
Harlen Kinsey <harlenkinsey60@gmail.com>
2023-10-23 17:34:22 -0500

    Removed blog and added "About Me" modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import M from 'materialize-css';
 
 const Header = () => {
 
     let width = window.innerWidth;
+
+    useEffect(() => {
+        var elems = document.querySelectorAll('.modal');
+        var instance = M.Modal.init(elems);
+    }) 
 
     return (
         <div className='row border-bottom black'>
@@ -12,15 +18,25 @@ const Header = () => {
                         <li className='header-text'>
                             <h3><span className='highlight'>Portfolio;</span></h3>
                         </li>
-                        <li className='blog-button'>
-                            <a href='https://blog.harlenkinsey.com' className={'btn-floating waves-effect waves-light white' + `${width >= 768 ? ' btn-large' : ' btn'}`}><i className='fas fa-blog black'></i></a>
-                        </li>
+                        
                     </div>
                 </div>
                 
                 <div className='col s6 right-align'>
                     <div>
                         <li className='header-button-initial'>
+                            <a className='waves-effect waves-light btn modal-trigger white black-text' href='#modal2'>About Me</a>
+                            <div id='modal2' className='modal left-align'>
+                                <div className='modal-content'>
+                                    <h4>About Me</h4>
+                                    <p>Hi, I'm Harlen 👋 I'm an undergraduate Accounting student with an interest in software development. Programming, automation, and data analysis are becoming increasingly important skills for accountants to have. Most of my experience has been with the MERN stack, Unity, and Godot. The languages I'm most familiar with are Python, C#, and JavaScript. If you ever feel the need to get in contact with me, you can shoot me an <a href='mailto:harlenkinsey60@gmail.com' className='blue-text'>email</a> or you can send me a message through <a href='https://www.linkedin.com/in/harlen-kinsey-9075291b1' className='blue-text'>LinkedIn</a>.</p>
+                                </div>
+                                <div className='modal-footer'>
+                                    <a href='#!' className='modal-close waves-effect waves-green btn-flat'>Close</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li className='header-button-subsequent'>
                             <a href='https://www.linkedin.com/in/harlen-kinsey-9075291b1' target='_blank' rel="noopener noreferrer" className={'btn-floating waves-effect waves-light white' + `${width >= 768 ? ' btn-large' : ' btn'}`}><i className='fab fa-linkedin black'></i></a>
                         </li>
                         <li className='header-button-subsequent'>
