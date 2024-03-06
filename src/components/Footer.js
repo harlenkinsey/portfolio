bd0ec29015bee5d6b7afc230481febe042050fe4
Harlen Kinsey <harlenkinsey60@gmail.com>
2024-03-06 14:08:45 -0600

    made date setting automatic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
+    
+    const today = new Date();
+    
     return (
         <div className='row black footer'>
             <div className='col s6 copyright'>
-                <b><p className='white-text'>Harlen Kinsey <i className='fas fa-copyright'></i> 2024</p></b>
+                <b><p className='white-text'>Harlen Kinsey <i className='fas fa-copyright'></i> {today.getFullYear()}</p></b>
             </div>
             <div className='col s6 right-align'>
                 <p className='white-text'>background image: u/astrellon3</p>
